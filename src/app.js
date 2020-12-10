17e8cf8cf6c3f05317eabecb3125da2b94e109f8
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-12-10 15:55:34 -0800

    added tech & cus folders

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-
 const { NODE_ENV } = require('./config');
 
 const app = express();
@@ -14,6 +13,8 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 app.use('/jobs', jobsRouter);
+app.use('/customers', customersRouter);
+app.use('/technicians', techniciansRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello, boilerplate!');
