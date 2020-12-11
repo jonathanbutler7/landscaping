4b8e3d980dec64e985291945181dd24f1a3a9449
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-12-11 12:34:36 -0800

    added Auth token

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const { NODE_ENV } = require('./config');
+const { NODE_ENV, API_TOKEN } = require('./config');
 
 const app = express();
 const jobsRouter = require('./routes/jobs-router');
@@ -15,13 +15,19 @@ const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.use((req, res, next) => {
+  const apiToken = API_TOKEN;
+  const authToken = req.get('Authorization');
+  if (!authToken || authToken !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  next();
+});
+
 app.use('/jobs', jobsRouter);
 app.use('/customers', customersRouter);
 app.use('/technicians', techniciansRouter);
-
-app.get('/', (req, res) => {
-  res.send('Hello, boilerplate!');
-});
 
 app.use((error, res) => {
   let response;
