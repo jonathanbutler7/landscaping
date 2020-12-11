e4a170d68a134c7e676e5339f7cf1cf25259c081
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-12-10 18:08:04 -0800

    added RUD to technicians and added async in getbyid

--- src/routes/technicians/technicians-router.js
+++ src/routes/technicians/technicians-router.js
@@ -11,18 +11,46 @@ techniciansRouter.route('/').get((req, res, next) => {
     })
     .catch(next);
 });
-techniciansRouter.put('/:id', jsonParser, (req, res, next) => {
-  const db = req.app.get('db');
-  const { id } = req.params;
-  const { body } = req;
-  db('technicians')
-    .where({ _id: id })
-    .update(body)
-    .returning('*')
-    .then((data) => {
-      res.send(data);
-    })
-    .catch(next);
-});
+
+techniciansRouter
+  .route('/:id')
+  .put(jsonParser, (req, res, next) => {
+    const { id } = req.params;
+    const db = req.app.get('db');
+    const { body } = req;
+    db('technicians')
+      .where({ _id: id })
+      .update(body)
+      .returning('*')
+      .then((data) => {
+        res.send(data);
+      })
+      .catch(next);
+  })
+  .get(async (req, res, next) => {
+    const { id } = req.params;
+    const db = req.app.get('db');
+    try {
+      const result = await db.from('technicians').where('_id', id).first();
+      res.send(result);
+    } catch (error) {
+      res.send(error);
+    }
+  })
+  .delete((req, res, next) => {
+    const db = req.app.get('db');
+    const { id } = req.params;
+    db('technicians')
+      .where({ _id: id })
+      .delete()
+      .returning('*')
+      .then((tech) => {
+        const techId = tech[0]._id;
+        res
+          .status(201)
+          .send({ message: `Deleted technician with id: ${techId}` });
+      })
+      .catch(next);
+  });
 
 module.exports = techniciansRouter;
