6987658d156611837d9058de63e43f59508c96c9
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-12-11 17:27:33 -0800

    added functions to fixtures files

---
+++ test/customers-fixtures.js
@@ -0,0 +1,30 @@
+function createCustomers() {
+  return [
+    {
+      _id: '361b1bee-6d99-44ea-a8a5-133648e184c9',
+      date_created: '2020-12-12T01:21:23.485Z',
+      name: 'Hank',
+      email: 'tech@tech.com',
+      phone: '193-133-1534',
+      address: '13 East Drive',
+    },
+    {
+      _id: '4214fb1a-40a3-41d9-957a-2cbabca5d9cd',
+      date_created: '2020-12-12T01:21:23.485Z',
+      name: 'Pam',
+      email: 'tech@tech.com',
+      phone: '125-173-1234',
+      address: '123 Orchard Avenue',
+    },
+    {
+      _id: '1ca0dadb-c957-451b-b465-a029f52c8401',
+      date_created: '2020-12-12T01:21:23.485Z',
+      name: 'Domino',
+      email: 'tech@tech.com',
+      phone: '623-423-1234',
+      address: '143 Grand St.',
+    },
+  ];
+}
+
+module.exports = { createCustomers };
