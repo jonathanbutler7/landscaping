3e3a7828afadf11c078f5a6f77948347a3a89381
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-12-27 19:21:04 -0600

    added form validation

--- src/components/CustomerForm.js
+++ src/components/CustomerForm.js
@@ -3,19 +3,32 @@ const { REACT_APP_SERVER_URL, REACT_APP_AUTH_KEY } = process.env;
 
 export default function CustomerForm() {
   const [customer, setCustomer] = useState({});
+  const [message, setMessage] = useState();
   function handleChange(e) {
     let newCustomer = customer;
     newCustomer[e.target.name] = e.target.value;
     setCustomer(newCustomer);
   }
 
-  async function handleSubmit(e) {
+  function handleValidate(e) {
     e.preventDefault();
+    if (
+      !customer.name ||
+      !customer.email ||
+      !customer.phone ||
+      !customer.address
+    ) {
+      setMessage('All fields are required.');
+    } else {
+      handleSubmit(e);
+    }
+  }
+
+  async function handleSubmit(e) {
     let url = `${REACT_APP_SERVER_URL}/customers`;
     var myHeaders = new Headers();
     myHeaders.append('Authorization', REACT_APP_AUTH_KEY);
     myHeaders.append('Content-Type', 'application/json');
-
     var raw = JSON.stringify(customer);
 
     var options = {
@@ -23,16 +36,19 @@ export default function CustomerForm() {
       headers: myHeaders,
       body: raw,
     };
-
-    const response = await fetch(url, options);
-    const result = await response.json();
-    console.log(result);
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      setMessage(`Successfully created customer ${result._id}`);
+    } catch (error) {
+      setMessage(error.status);
+    }
   }
-
+  console.log(message);
   return (
     <div>
       <h3>Customer form</h3>
-      <form action='' onSubmit={handleSubmit}>
+      <form action='' onSubmit={handleValidate}>
         <label htmlFor=''>Name:</label>
         <input onChange={(e) => handleChange(e)} name='name' type='text' />
         <br />
@@ -45,6 +61,7 @@ export default function CustomerForm() {
         <label htmlFor=''>Address:</label>
         <input onChange={(e) => handleChange(e)} name='address' type='text' />
         <br />
+        {message && <p>{message}</p>}
         <button type='submit'>Submit</button>
       </form>
     </div>
