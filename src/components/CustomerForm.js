9a4585646481c801bf7108b2e08ef84eb7cee615
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-12-27 19:14:46 -0600

    customerform working

--- src/components/CustomerForm.js
+++ src/components/CustomerForm.js
@@ -1,17 +1,32 @@
 import React, { useState } from 'react';
+const { REACT_APP_SERVER_URL, REACT_APP_AUTH_KEY } = process.env;
 
 export default function CustomerForm() {
   const [customer, setCustomer] = useState({});
-
   function handleChange(e) {
     let newCustomer = customer;
     newCustomer[e.target.name] = e.target.value;
     setCustomer(newCustomer);
   }
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
-    console.log(customer);
+    let url = `${REACT_APP_SERVER_URL}/customers`;
+    var myHeaders = new Headers();
+    myHeaders.append('Authorization', REACT_APP_AUTH_KEY);
+    myHeaders.append('Content-Type', 'application/json');
+
+    var raw = JSON.stringify(customer);
+
+    var options = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+    };
+
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result);
   }
 
   return (
