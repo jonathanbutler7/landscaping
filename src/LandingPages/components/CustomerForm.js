4519016df148f421708490f0e94e883295ef55ed
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-12-29 18:45:36 -0800

    got autocomplete working with material UI

--- src/LandingPages/components/CustomerForm.js
+++ src/LandingPages/components/CustomerForm.js
@@ -1,26 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useLandscaping } from '../context';
 import { validateCustomer } from '../helpers/helpers';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import TextField from '@material-ui/core/TextField';
 import axios from 'axios';
+
 export default function CustomerForm() {
   const { serverUrl, authKey, setNewCustomer, mapKey } = useLandscaping();
   const [customer, setCustomer] = useState({});
   const [message, setMessage] = useState(null);
-  const [smartAdd, setSmartAdd] = useState(null);
   const [autoComplete, setAutoComplete] = useState([]);
 
   function handleChange(e) {
     let newCustomer = customer;
-    newCustomer[e.target.name] = e.target.value;
-    console.log(newCustomer);
+    if (e.target.innerText) {
+      newCustomer.address = e.target.innerText;
+    } else {
+      newCustomer[e.target.name] = e.target.value;
+    }
     setCustomer(newCustomer);
   }
+  console.log(customer);
 
-  function handleSmartChange(e) {
-    let newCustomer = customer;
-    newCustomer.address = e.target.innerText;
-    console.log(newCustomer);
-    setCustomer(newCustomer);
+  async function searchAddress(e) {
+    handleChange(e);
+    const val = e.target.value;
+    const mapquestUrl = 'http://www.mapquestapi.com/search/v3';
+    console.log('searching', val);
+    if (val.length > 2) {
+      const url = `${mapquestUrl}/prediction?key=${mapKey}&limit=5&collection=adminArea,poi,address,category,franchise,airport&q=${val}`;
+      try {
+        const response = await axios.get(url);
+        const result = response.data.results;
+        setAutoComplete(result);
+      } catch (error) {
+        console.error(error);
+      }
+    }
   }
 
   function handleValidate(e) {
@@ -38,7 +54,7 @@ export default function CustomerForm() {
     myHeaders.append('Authorization', authKey);
     myHeaders.append('Content-Type', 'application/json');
     var raw = JSON.stringify(customer);
-
+    console.log(customer)
     var options = {
       method: 'POST',
       headers: myHeaders,
@@ -53,21 +69,7 @@ export default function CustomerForm() {
       setMessage(error.status);
     }
   }
-
-  async function searchAddress(val) {
-    const mapquestUrl = 'http://www.mapquestapi.com/search/v3';
-    if (val.length > 2) {
-      const url = `${mapquestUrl}/prediction?key=${mapKey}&limit=5&collection=adminArea,poi,address,category,franchise,airport&q=${val}`;
-      try {
-        const response = await axios.get(url);
-        const result = response.data.results;
-        setAutoComplete(result);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  }
-  console.log(customer);
+  const { name, phone, email, address } = customer;
   return (
     <div>
       <h3>Enter your details to be taken to an order page.</h3>
@@ -81,25 +83,22 @@ export default function CustomerForm() {
         <label htmlFor=''>Email:</label>
         <input onChange={(e) => handleChange(e)} name='email' type='text' />
         <br />
-        <label htmlFor=''>Address:</label>
-        <input onChange={(e) => handleChange(e)} name='address' type='text' />
-        <br />
-        <label htmlFor=''>Smart address:</label>
-        <input
-          onChange={(e) => searchAddress(e.target.value)}
-          name='address'
-          type='text'
+        <Autocomplete
+          id='combo-box-demo'
+          options={autoComplete}
+          getOptionLabel={(option) => option.displayString}
+          style={{ width: 300 }}
+          onInputChange={(e) => searchAddress(e)}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label='Combo box'
+              variant='outlined'
+              onInputChange={(e) => searchAddress(e)}
+            />
+          )}
         />
         <br />
-        {autoComplete.map((result, idx) => (
-          <p
-            key={idx}
-            style={{ margin: '0' }}
-            onClick={(e) => handleSmartChange(e)}
-          >
-            {result.displayString}
-          </p>
-        ))}
         {message && <p>{message}</p>}
         <button type='submit'>Submit</button>
       </form>
