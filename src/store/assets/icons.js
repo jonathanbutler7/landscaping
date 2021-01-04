e228a80d6baff4b4f8245dc6102b8e1dd989479b
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-01-03 21:10:42 -0800

    added icons to assets

---
+++ src/store/assets/icons.js
@@ -0,0 +1,41 @@
+import { GoQuote } from 'react-icons/go';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import {
+  AiFillStar,
+  AiOutlineStar,
+  AiOutlineMail,
+  AiOutlineClose,
+} from 'react-icons/ai';
+import {
+  BsChevronUp,
+  BsChevronDown,
+  BsChevronLeft,
+  BsChevronRight,
+} from 'react-icons/bs';
+import {
+  FaPhoneAlt,
+  FaInstagram,
+  FaFacebookF,
+  FaTwitter,
+  FaArrowLeft,
+  FaArrowRight,
+} from 'react-icons/fa';
+
+export {
+  GoQuote,
+  GiHamburgerMenu,
+  FaPhoneAlt,
+  AiFillStar,
+  AiOutlineStar,
+  AiOutlineClose,
+  FaInstagram,
+  FaFacebookF,
+  FaTwitter,
+  AiOutlineMail,
+  FaArrowLeft,
+  FaArrowRight,
+  BsChevronUp,
+  BsChevronDown,
+  BsChevronLeft,
+  BsChevronRight,
+};
--- src/store/assets/illustrations.js
+++ src/store/assets/illustrations.js
@@ -5,5 +5,19 @@ const img2 = `${imgurUrl}/bWwyUvE.png`;
 const img3 = `${imgurUrl}/Q9PrwbS.png`;
 const img4 = `${imgurUrl}/DzpDHmk.png`;
 const img5 = `${imgurUrl}/raboW8U.png`;
+const phoneImg = `${imgurUrl}/AXhrMUg.png`;
+const geometry1 = `${imgurUrl}/Ugf9WoH.png`;
+const geometry2 = `${imgurUrl}/H7KoKCL.png`;
+const geometricGirl = `${imgurUrl}/ITLKoG5.png`;
 
-export { img1, img2, img3, img4, img5 };
+export {
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  phoneImg,
+  geometry1,
+  geometry2,
+  geometricGirl,
+};
