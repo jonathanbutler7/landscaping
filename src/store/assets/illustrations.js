6a09a95e80aa7a41ee7a8cfadc2c34c27cce2686
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-01-03 20:47:29 -0800

    added assets and content

---
+++ src/store/assets/illustrations.js
@@ -0,0 +1,9 @@
+const imgurUrl = 'https://i.imgur.com';
+
+const img1 = `${imgurUrl}/DzpDHmk.png`;
+const img2 = `${imgurUrl}/bWwyUvE.png`;
+const img3 = `${imgurUrl}/Q9PrwbS.png`;
+const img4 = `${imgurUrl}/DzpDHmk.png`;
+const img5 = `${imgurUrl}/raboW8U.png`;
+
+export { img1, img2, img3, img4, img5 };
---
+++ src/store/content/reviews.js
@@ -0,0 +1,52 @@
+const reviews = [
+  {
+    subject: 'Amazing service!',
+    review:
+      'I had an excellent experience with Los Angeles Gardening & Landscaping. They came to mulch my flower beds last spring and did a great job, and were very easy to get in touch with. Thanks for the amazing service!',
+    rating: 5,
+    name: 'Joanna F.',
+    location: 'Petaluma, CA',
+  },
+  {
+    subject: 'Highly recommended',
+    review:
+      'I had an excellent experience with Los Angeles Gardening & Landscaping. They came to mulch my flower beds last spring and did a great job, and were very easy to get in touch with. Thanks for the amazing service!',
+    rating: 5,
+    name: 'Hal R.',
+    location: 'Haslett, CA',
+  },
+  {
+    subject: 'Convenient process',
+    review:
+      'I had an excellent experience with Los Angeles Gardening & Landscaping. They came to mulch my flower beds last spring and did a great job, and were very easy to get in touch with. Thanks for the amazing service!',
+    rating: 5,
+    name: 'Pass C.',
+    location: 'Pasadena, CA',
+  },
+  {
+    subject: 'Attentive to my needs',
+    review:
+      'I had an excellent experience with Los Angeles Gardening & Landscaping. They came to mulch my flower beds last spring and did a great job, and were very easy to get in touch with. Thanks for the amazing service!',
+    rating: 5,
+    name: 'Jose G.',
+    location: 'Palo Pinto, CA',
+  },
+  {
+    subject: 'Above and beyond',
+    review:
+      'I had an excellent experience with Los Angeles Gardening & Landscaping. They came to mulch my flower beds last spring and did a great job, and were very easy to get in touch with. Thanks for the amazing service!',
+    rating: 5,
+    name: 'Monica C.',
+    location: 'Santa Monica, CA',
+  },
+  {
+    subject: 'Great communication',
+    review:
+      'I had an excellent experience with Los Angeles Gardening & Landscaping. They came to mulch my flower beds last spring and did a great job, and were very easy to get in touch with. Thanks for the amazing service!',
+    rating: 5,
+    name: 'Vinny B.',
+    location: 'Venice, CA',
+  },
+];
+
+export { reviews };
