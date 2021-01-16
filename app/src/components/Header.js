67fb7bbf82824ff121bd30b30f7d8896c1d99274
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-01-15 16:49:06 -0800

    makes navbar hamburger menu responsive

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import classes from './Header.module.scss';
+import { Link } from 'react-router-dom';
 import { GiHamburgerMenu, FaPhoneAlt } from '../store/index';
 
 export default function Header() {
   return (
-      <header className={classes.main}>
-        <div className={classes.left}>
-          <h1 className={classes.title}>🏡 LA Landscaping and gardening</h1>
-        </div>
-        <div className={classes.right}>
+    <header className={classes.main}>
+      <div className={classes.left}>
+        <h1 className={classes.title}>🏡 LA Landscaping and gardening</h1>
+      </div>
+      <div className={classes.right}>
+        <div className={classes.menuContent}>
+          <Link to='/worker'>become a worker</Link>
           <FaPhoneAlt />
-          <p>(123) 456-7890</p>
-          <GiHamburgerMenu />
+          <a href='tel:123-123-1234'>(123) 456-7890</a>
+          <button className='light-button'>Place an order</button>
         </div>
-      </header>
-    
+        <GiHamburgerMenu className={classes.hamburger} />
+      </div>
+    </header>
   );
 }
