a63be7def49d016e68b80f4862955a01b3aa9eea
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-01-16 20:06:15 -0800

    styles CTA components

--- app/src/LandingPage/components/CTAs.js
+++ app/src/LandingPage/components/CTAs.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import classes from './CTAs.module.scss';
-import { grayBlock, img3, geometry2, ctas } from '../../store/index';
+import { grayBlock, ctas } from '../../store/index';
 
 export default function CTAs() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
+    <div className={classes.main}>
       {ctas.map((cta, i) => (
         <div className={classes.cta}>
-          <div className={classes.textBlock}>
-            <h1>{cta.title}</h1>
-            <p>{cta.content}</p>
-            <button className='light-button'>{cta.button}</button>
-          </div>
-          <img src={cta.img} alt={cta.img} style={{ width: '50%' }} />
+          {i % 2 === 0 ? (
+            <>
+              <div className={classes.textBlock}>
+                <h1>{cta.title}</h1>
+                <p>{cta.content}</p>
+                <button className='light-button'>{cta.button}</button>
+              </div>
+              <img src={cta.img} alt={cta.img} style={{ width: '50%' }} />
+            </>
+          ) : (
+            <>
+              <img src={cta.img} alt={cta.img} style={{ width: '50%' }} />
+              <div className={classes.textBlock}>
+                <h1>{cta.title}</h1>
+                <p>{cta.content}</p>
+                <button className='light-button'>{cta.button}</button>
+              </div>
+            </>
+          )}
         </div>
       ))}
       <img src={grayBlock} alt='' style={{ margin: '0 auto' }} />
--- app/src/store/content/content/CTAs.js
+++ app/src/store/content/content/CTAs.js
@@ -1,24 +1,26 @@
-import { geometry2, img3 } from '../../assets';
+import { geometry2, img3, phoneImg } from '../../assets';
 
 const ctas = [
   {
     title: 'Free quote',
-    content: 'Quotes are upfront and prices are guaranteed',
-    button: 'Get a quote today',
+    content:
+      'Quotes are upfront, prices are guaranteed, and we will wow you with our customer service',
+    button: 'Get a quote',
     img: img3,
   },
   {
     title: 'Satisfaction guaranteed',
     content:
-      "buttonhundreds of great reviews don't convince you, let us go the extra mile",
+      "If hundreds of great reviews don't convince you, let us go the extra mile",
     button: 'Give us a try',
     img: geometry2,
   },
   {
     title: 'Excellent communication',
-    content: 'Our customers say we are super easy to get ahold of',
-    button: 'Give us a call',
-    img: img3,
+    content:
+      'Our customers say we are super easy to get ahold of, why not give us a call now',
+    button: 'Call now',
+    img: phoneImg,
   },
 ];
 
