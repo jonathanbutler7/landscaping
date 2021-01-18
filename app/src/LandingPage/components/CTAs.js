4e2db7674c879ca93119a950c1560b9e6d051903
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-01-17 16:25:02 -0800

    makes CTAs responsive

--- app/src/LandingPage/components/CTAs.js
+++ app/src/LandingPage/components/CTAs.js
@@ -4,34 +4,36 @@ import { grayBlock, ctas } from '../../store/index';
 
 export default function CTAs() {
   return (
-    <div className={classes.main}>
-      {ctas.map((cta, i) => (
-        <>
-          {i % 2 === 0 ? (
-            <div className={classes.cta}>
-              <div className={classes.textBlock}>
-                <h1>{cta.title}</h1>
-                <p>{cta.content}</p>
-                <button className='light-button'>{cta.button}</button>
+    <>
+      <div className={classes.main}>
+        {ctas.map((cta, i) => (
+          <>
+            {i % 2 === 0 ? (
+              <div className={classes.cta}>
+                <div className={classes.textBlock}>
+                  <h1>{cta.title}</h1>
+                  <p>{cta.content}</p>
+                  <button className='light-button'>{cta.button}</button>
+                </div>
+                <img src={cta.img} alt={cta.img} style={{ width: '50%' }} />
               </div>
-              <img src={cta.img} alt={cta.img} style={{ width: '50%' }} />
-            </div>
-          ) : (
-            <div
-              className={classes.cta}
-              style={{ background: 'rgb(239 236 236 / 21%)' }}
-            >
-              <img src={cta.img} alt={cta.img} style={{ width: '50%' }} />
-              <div className={classes.textBlock}>
-                <h1>{cta.title}</h1>
-                <p>{cta.content}</p>
-                <button className='light-button'>{cta.button}</button>
+            ) : (
+              <div
+                className={classes.cta}
+                style={{ background: 'rgb(239 236 236 / 21%)' }}
+              >
+                <img src={cta.img} alt={cta.img} style={{ width: '50%' }} />
+                <div className={classes.textBlock}>
+                  <h1>{cta.title}</h1>
+                  <p>{cta.content}</p>
+                  <button className='light-button'>{cta.button}</button>
+                </div>
               </div>
-            </div>
-          )}
-        </>
-      ))}
+            )}
+          </>
+        ))}
       <img src={grayBlock} alt='' style={{ margin: '0 auto' }} />
-    </div>
+      </div>
+    </>
   );
 }
