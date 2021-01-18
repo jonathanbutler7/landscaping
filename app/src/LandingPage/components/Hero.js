630f3694781cfdfcd2f3e5fe26ac03b8364e20a2
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-01-17 17:30:16 -0800

    changes hero font sizes

--- app/src/LandingPage/components/Hero.js
+++ app/src/LandingPage/components/Hero.js
@@ -6,7 +6,7 @@ export default function Hero({ service }) {
   return (
     <div className={classes.main}>
       <div className={classes.top}>
-        <h1>{service.name} services</h1>
+        <h1>{service.name} services, reimagined</h1>
         <p>{service.description}</p>
         <button className='light-button'>Book a service</button>
       </div>
